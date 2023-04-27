4017491a10c0662cc159d836f5c4cb8dba752cd5
Matthew Hayward <hayward.m.d@hotmail.co.uk>
2023-04-27 10:52:45 +0100

    Added entity helper with cube vertices.

---
+++ Scene/EntityHelper.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Scene\Entity.h"
+
+namespace scene
+{
+
+namespace helper
+{
+
+const Entity::VertexLit* GetCubeVertices( UINT& numVertices );
+
+} // namespace helper
+
+} // namespace scene
